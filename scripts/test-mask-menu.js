f0116556a204301cb374546d855138fc516978b1
OMEGI <lola.othon@gmail.com>
2025-10-29 15:18:51 +0100

    Feat/test mask menu (add homepage) (#17)
    * feat test-mask-menu: POC;
    
    * Fix homepage
    
    Code
    
    * Fix homepage: le css
    
    * fix css adding vidéo
    
    * css fix, vidéo tuto
    
    * css images/ vidéo dossier
    
    * liées brush.html sur js
    
    ---------
    
    Co-authored-by: LucienBS <79203857+Lucien77144@users.noreply.github.com>

---
+++ scripts/test-mask-menu.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded')
+  let homepage = document.querySelector("#homepage")
+  let SideChapelleHomepage = document.querySelector("#chapelle-div");
+SideChapelleHomepage.addEventListener('click', () => {
+  let videoTuto = document.createElement('video');
+  videoTuto.src = '../public/videos/video_tuto.mp4';
+  videoTuto.autoplay = true;
+  videoTuto.style.position = "fixed";
+  videoTuto.style.top = 0;
+  videoTuto.style.left = 0;
+  videoTuto.style.width = '100%';
+  videoTuto.style.height = '100%';
+  videoTuto.style.zIndex = 1000;
+  videoTuto.style.backgroundColor = 'black';
+  homepage.style.display = 'none';
+
+  videoTuto.addEventListener('ended', () => {
+    document.body.removeChild(videoTuto);
+    window.location.href = 'html/brush.html';
+
+
+  });
+
+  document.body.appendChild(videoTuto);
+  });
+  
+})
+
