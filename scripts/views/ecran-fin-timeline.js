ed50cbae820dfcef71bd0177464b9aa7324d597f
New <naths.raf@gmail.com>
2025-10-31 15:06:57 +0100

    Feat/7.1 ecran fin timeline (#36)
    * feat: ecran-fin-timeline: add json data and initialisation of html file
    
    * adding fonts and alignment
    
    * fix: font sizes and line height
    
    * change
    
    * add test link
    
    * fixing html place in tree
    
    * Fixing button path
    
    ---------
    
    Co-authored-by: paulinovart <pauline.dierick@gmail.com>

---
+++ scripts/views/ecran-fin-timeline.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     fetch('public/data/ecran-fin-timeline.json')
+//       .then((response) => response.json())
+//       .then((data) => {
+//         document.querySelector('#title').textContent = data.title;
+  
+//         gsap.fromTo(
+//           '#title',
+//           {
+//             opacity: 0,
+//           },
+//           {
+//             duration: 3,
+//             opacity: 1,
+//           }
+//         );
+//       });
+//   });
+
+
+
+
